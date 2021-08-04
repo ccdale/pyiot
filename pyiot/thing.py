27dd4153f63cafb7ab3f4fc4b1f280452764e42d
ccdale <chris.allison@hivehome.com>
2021-08-04 15:17:29 +0100

    pyiot lesson 1

---
+++ pyiot/thing.py
@@ -0,0 +1,10 @@
+class Thing:
+    def __init__(self, name="thing", xtype="thing"):
+        self.name = name
+        self.xtype = xtype
+
+    def __repr__(self):
+        return f"<Thing(name={self.name}, xtype={self.xtype})>"
+
+    def __str__(self):
+        return self.name
