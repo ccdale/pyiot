2176f70546da9ad4ddbecf3db1e66813f5ddbc6e
ccdale <chris.allison@hivehome.com>
2021-08-25 14:51:40 +0100

    use better __repr__

--- tests/test_thing.py
+++ tests/test_thing.py
@@ -6,12 +6,12 @@ from pyiot.thing import Thing, WindowSensor
 
 def test_Thing():
     th = Thing()
-    assert f"{th}" == "<Thing(name=thing, xtype=thing)>"
+    assert f"{th}" == "Thing(name=thing, xtype=thing)"
 
 
 def test_Thing_named():
     th = Thing(name="thething")
-    assert f"{th}" == "<Thing(name=thething, xtype=thing)>"
+    assert f"{th}" == "Thing(name=thething, xtype=thing)"
 
 
 def test_Thing_invalid_arg():
@@ -21,12 +21,12 @@ def test_Thing_invalid_arg():
 
 def test_wsensor():
     ws = WindowSensor()
-    assert f"{ws}" == "<WindowSensor(name=wsensor, xtype=sensor)>"
+    assert f"{ws}" == "WindowSensor(name=wsensor, xtype=sensor)"
 
 
 def test_wsensor_with_type():
     ws = WindowSensor(xtype="thingy")
-    assert f"{ws}" == "<WindowSensor(name=wsensor, xtype=thingy)>"
+    assert f"{ws}" == "WindowSensor(name=wsensor, xtype=thingy)"
 
 
 def test_ws_status():
